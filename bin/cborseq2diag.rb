00359ea896dfc4152f0f76f6fdd72ba343fc4081
Carsten Bormann <cabo@tzi.org>
2021-01-23 23:15:50 +0100

    0.5.10: add cborseq2diag.rb

---
+++ bin/cborseq2diag.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+require 'cbor-diagnostic'
+
+options = ''
+while /\A-([et]+)\z/ === ARGV[0]
+  options << $1
+  ARGV.shift
+end
+
+ARGF.binmode
+i = ARGF.read
+while !i.empty?
+  o, i = CBOR.decode_with_rest(i)
+  out = o.cbor_diagnostic(try_decode_embedded: /e/ === options, bytes_as_text: /t/ === options)
+  if i.empty?
+    puts out
+  else
+    print out << ', '
+  end
+end
