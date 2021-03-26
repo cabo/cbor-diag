eb6cc6ff3f514cc8703364e4b267493e958d27f5
Carsten Bormann <cabo@tzi.org>
2021-03-26 19:03:12 +0100

    0.5.18: Add diag2diag.rb

---
+++ bin/diag2diag.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require 'cbor-pure'
+require 'treetop'
+require 'cbor-diag-parser'
+unless ''.respond_to? :b
+  require 'cbor-diagnostic'     # for .b
+end
+require 'cbor-pretty'
+require 'cbor-diagnostic'
+require 'cbor-deterministic'
+require 'cbor-canonical'
+
+options = ''
+while /\A-([cdetu]+)\z/ === ARGV[0]
+  options << $1
+  ARGV.shift
+end
+
+def diagnostic(o, options)
+  o = o.cbor_pre_canonicalize if /c/ === options
+  o = o.cbor_prepare_deterministic if /d/ === options
+  puts o.cbor_diagnostic(try_decode_embedded: /e/ === options,
+                         bytes_as_text: /t/ === options,
+                         utf8: /u/ === options)
+end
+
+parser = CBOR_DIAGParser.new
+
+i = ARGF.read.b                 # binary to work around treetop performance bug
+if result = parser.parse(i)
+  decoded = result.to_rb
+  out = case decoded
+        when CBOR::Sequence
+          decoded.elements
+        else
+          decoded
+        end.map {|x| diagnostic(x, options)}.join(", ")
+  print out
+else
+  puts "*** can't parse #{i}"
+  puts "*** #{parser.failure_reason}"
+end
