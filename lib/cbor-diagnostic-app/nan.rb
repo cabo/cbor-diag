2493b5565d43db6f706b49e7cb409c747e7d6659
Carsten Bormann <cabo@tzi.org>
2024-08-07 13:39:08 +0200

    Work around %a bug in older rubies

--- lib/cbor-diagnostic-app/nan.rb
+++ lib/cbor-diagnostic-app/nan.rb
@@ -2,7 +2,7 @@ require 'scanf'
 
 class CBOR_DIAG::App_nan
   def self.decode(_, s)
-    val, = s.scanf("%a")
+    val, = s.sub("p0", "p+0").scanf("%a")
     raise ArgumentError.new ("nan'#{s}' not valid") unless val &&
                                                            val.abs > 1.0 && val.abs < 2.0
     val.cbor_nan_toggle
