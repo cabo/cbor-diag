624e927af08349226d41a0ba1a4900c4b6283288
Carsten Bormann <cabo@tzi.org>
2026-04-10 20:17:17 +0200

    0.11.4: fix mixed types in bytes<<>>

--- lib/cbor-diagnostic-app/bytes.rb
+++ lib/cbor-diagnostic-app/bytes.rb
@@ -2,14 +2,16 @@ require 'cbor-deterministic'
 
 class CBOR_DIAG::App_bytes
   def self.decode(_, s)
-    if CBOR::Sequence === s
-      a = s.elements
-    else
-      a = [s]
-    end
-    if a.detect { |el| not String === el }
-      raise ArgumentError.new("cbor-diagnostic: bytes<<#{a.cbor_diagnostic[1...-1]}>>: not all strings: Argument Error")
-    end
-    a.join.b
+    a = if CBOR::Sequence === s
+          s.elements
+        else
+          [s]
+        end
+    a.map { |el|
+      unless String === el
+        raise ArgumentError.new("cbor-diagnostic: bytes<<#{a.cbor_diagnostic[1...-1]}>>: #{el.cbor_diagnostic} not a string: Argument Error")
+      end
+      el.b
+    }.join.b # .b needed so empty array becomes byte string, too
   end
 end
